529d27d78cecbbe83bdeb99ec4d72bae890f081a
Siyeong-Lee <siyeong@sogang.ac.kr>
2018-01-08 14:09:22 +0900

    [init] skeletoning

---
+++ src/image.c
@@ -0,0 +1,12 @@
+#include "image.h"
+#include "utils.h"
+#include "blas.h"
+//#include "cuda.h"
+#include <stdio.h>
+#include <math.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
